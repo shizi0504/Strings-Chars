b8e4403f80017f62096979dbc98089695960d9a4
shizi0504 <81694117+shizi0504@users.noreply.github.com>
2023-07-07 23:21:41 +0530

    Update removeAdjacentCharacters.cpp

--- removeAdjacentCharacters.cpp
+++ removeAdjacentCharacters.cpp
@@ -20,3 +20,36 @@ int main()
     cout<<removeAdjchar(str);
     return 0 ;
 }
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void removeAdjchar(string& s)
+{
+    int writeIndex = 0; // Index to track the current position to write in the string
+
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] != s[writeIndex]) // Check if the current character is different from the character at writeIndex
+        {
+            writeIndex++;
+            s[writeIndex] = s[i]; // Overwrite the character at writeIndex with the current character
+        }
+    }
+
+    s.resize(writeIndex + 1); // Resize the string to remove any leftover characters
+}
+
+int main()
+{
+    string str;
+    cout << "Enter the string: ";
+    getline(cin, str);
+    cout << "The string is: " << str << endl;
+    removeAdjchar(str);
+    cout << "Modified string: " << str << endl;
+    return 0;
+}
